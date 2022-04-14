ac1c1cbebf8e655c3ed27b48eae2d20d57c7ac1b
justinablair <justinablair3@icloud.com>
2022-04-14 12:59:43 +0100

    api code improvements

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 /*user list */
 const input = document.querySelector("#js-form input");
-const myUlDisplay = document.querySelector("#myUl");
-/*api list */
-const todoSection = document.querySelector("#todo-section");
+const uiDisplay = document.querySelector("#myUl");
 const todoTemplate = document.querySelector("#todo-template");
 
 function handleFormSubmission(e) {
@@ -10,50 +8,41 @@ function handleFormSubmission(e) {
   if (input.value.length === 0) {
     alert("Please enter a task");
   } else {
-    const DOMobject = document.createElement("li");
-    DOMobject.innerText = input.value;
-    myUlDisplay.appendChild(DOMobject);
+    const domObject = document.createElement("li");
+    domObject.innerText = input.value;
+    uiDisplay.appendChild(domObject);
   }
- // return false; //do not submit the form
+  // return false; //do not submit the form
 }
 
 const url = "https://jsonplaceholder.typicode.com/users/1/todos";
 
-
-
 async function getData() {
   const todoData = await fetch(url);
   const todos = await todoData.json();
-  
+
   todos.forEach((todo) => {
     const title = todo.title;
     const completed = todo.completed;
 
-      
-        const newTodo = document.importNode(todoTemplate.content, true);
-        const todoTitle = newTodo.querySelector(".todo-title");
-        const todoCompleted = newTodo.querySelector(".todo-completed");
-        todoTitle.innerText = title;
-        todoCompleted.innerText = completed;
-        todoSection.appendChild(newTodo);
-      });
-    }
+    //li to contain fetched todo items
+    const apiLi = document.createElement("li");
+    uiDisplay.appendChild(apiLi);
+
+    const newTodo = document.importNode(todoTemplate.content, true);
+
+    //h3 element placeholder added to fetched api li
+    const todoTitle = document.createElement("h3");
+    apiLi.appendChild(todoTitle); //h3 added to li
+
+    //p element placeholder added to fetched api li
+    const todoCompleted = document.createElement("p");
+    apiLi.appendChild(todoCompleted);
+
+    //title and completed placeholders replaced with api data
+    todoTitle.innerText = title;
+    todoCompleted.innerText = completed;
+    uiDisplay.appendChild(newTodo); //todoTemplate added to ui display
+  });
+}
 getData().catch((err) => console.error(err));
-
-
-
-
-// function deleting (){
-
-//     const deleteButton = document.createElement("button"); //create new button element
-//     deleteButton.setAttribute("class", "btn"); //set button class to be btn
-//     deleteButton.textContent="Delete";//set button text to say delete
-//     console.log(deleteButton);
-//    deleteButton.style.display = 'none'
-//    button.addEventListener('click', function(event) {
-
-//     deleteButton.style.display = 'block'
-
-//   })
-// }
-// deleting ();

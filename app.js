fe0c7c95e2e890949c5efbe8e32ffbbc1c3c1597
justinablair <57632945+justinablair@users.noreply.github.com>
2022-04-14 15:46:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
 /*user list */
 const input = document.querySelector("#js-form input");
-const myUlDisplay = document.querySelector("#myUl");
-/*api list */
-const todoSection = document.querySelector("#todo-section");
+const ulDisplay = document.querySelector("#myUl");
 const todoTemplate = document.querySelector("#todo-template");
 
 function handleFormSubmission(e) {
-  e.preventdefault();
   if (input.value.length === 0) {
     alert("Please enter a task");
   } else {
-    const DOMobject = document.createElement("li");
-    DOMobject.innerText = input.value;
-    myUlDisplay.appendChild(DOMobject);
+
+    const domObject = document.createElement("li");
+    domObject.innerText = input.value;
+
+    const deleteButton = document.createElement("button"); //create new button element
+    deleteButton.setAttribute("class", "btn"); //set button class to be btn
+    deleteButton.appendChild(document.createTextNode("x")); //set button text to say delete
+    domObject.appendChild(deleteButton);
+    ulDisplay.prepend(domObject); //List item+delete button added to top of Ui display 
+    
   }
- // return false; //do not submit the form
+
+  e.preventDefault();   // return false; //do not submit the form
 }
 
-const url = "https://jsonplaceholder.typicode.com/users/1/todos";
 
+var form = document.getElementById("js-form");
+form.addEventListener("submit", handleFormSubmission, true); // attach event listener
+
+const url = "https://jsonplaceholder.typicode.com/users/1/todos";
 
 async function getData() {
   const todoData = await fetch(url);
   const todos = await todoData.json();
-  
+
   todos.forEach((todo) => {
     const title = todo.title;
     const completed = todo.completed;
 
-      
-        const newTodo = document.importNode(todoTemplate.content, true);
-        const todoTitle = newTodo.querySelector(".todo-title");
-        const todoCompleted = newTodo.querySelector(".todo-completed");
-        todoTitle.innerText = title;
-        todoCompleted.innerText = completed;
-        todoSection.appendChild(newTodo);
-      });
-    }
-  
+    //li to contain fetched todo items
+    const apiLi = document.createElement("li");
+    ulDisplay.appendChild(apiLi);
+
+    const newTodo = document.importNode(todoTemplate.content, true);
+
+    //h3 element placeholder added to fetched api li
+    const todoTitle = document.createElement("h3");
+    apiLi.appendChild(todoTitle); //h3 added to li
+
+    //p element placeholder added to fetched api li
+    const todoCompleted = document.createElement("p");
+    apiLi.appendChild(todoCompleted);
+
+    //title and completed placeholders replaced with api data
+    todoTitle.innerText = title;
+    todoCompleted.innerText = completed;
+    ulDisplay.appendChild(newTodo); //todoTemplate added to ui display
+  });
+}
 getData().catch((err) => console.error(err));
-
-// function deleting (){
-
-//     const deleteButton = document.createElement("button"); //create new button element
-//     deleteButton.setAttribute("class", "btn"); //set button class to be btn
-//     deleteButton.textContent="Delete";//set button text to say delete
-//     console.log(deleteButton);
-//    deleteButton.style.display = 'none'
-//    button.addEventListener('click', function(event) {
-
-//     deleteButton.style.display = 'block'
-
-//   })
-// }
-// deleting ();
